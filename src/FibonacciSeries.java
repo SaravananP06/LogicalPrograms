0577f19c17d1c176a057878c4da0938635b48224
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-06-29 11:09:35 +0530

    [Saravanan]: Added Fibonacci Series Program

--- src/FibonacciSeries.java
+++ src/FibonacciSeries.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
-        System.out.println("Welcome to Logical Programs");
+        System.out.println("Welcome to Fibonacci Series Program");
+
+        // initializing variables
+        int n1 = 0, n2 = 1, n3 , i;
+        // taking input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int num = sc.nextInt();
+
+        System.out.println(n1+ " " + n2);
+
+        for (i=2; i<num; i++){
+            n3 = n1 + n2;
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+        }
+
     }
 }
