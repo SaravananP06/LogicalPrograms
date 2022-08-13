26ca757ec99af880ebc4260c3c54b17f2ff60f5e
SaravananP06 <saravanan.palaniswami6@gmail.com>
2022-08-13 18:20:07 +0530

    [Saravanan]: Added program to generate coupon number using random

---
+++ src/CouponNumber.java
@@ -0,0 +1,29 @@
+public class CouponNumber {
+    /*
+    Given N distinct Coupon Numbers, how many random numbers do you
+    need to generate a distinct coupon number? This program simulates this random
+    process.
+     */
+
+    public static void main(String[] args) {
+        /*
+        Initialising the variables
+         */
+        char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+        int max=100000000;
+        int random=(int) (Math.random()*max);
+        StringBuffer sb=new StringBuffer();
+
+        /*
+        while loop
+         */
+        while (random>0)
+        {
+            sb.append(chars[random % chars.length]);
+            random /= chars.length;
+        }
+
+        String couponCode=sb.toString();
+        System.out.println("Coupon Code: "+couponCode);
+    }
+}
